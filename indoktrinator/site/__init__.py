a01c2926f1ac1a566877914a58eb9be59bf23e55
Jan Dvořák <jan.dvorak@singularita.net>
2017-01-19 10:19:01 +0100

    site: clean up module imports
    Signed-off-by: Jan Dvořák <jan.dvorak@singularita.net>

--- indoktrinator/site/__init__.py
+++ indoktrinator/site/__init__.py
@@ -3,26 +3,22 @@
 
 from twisted.python import log
 
-from sqlalchemy import *
-from sqlalchemy.exc import *
-from werkzeug.exceptions import *
+from werkzeug.exceptions import Forbidden
+from flask_cors import CORS
+from flask import Flask, Response, request, render_template, jsonify, \
+                  url_for, send_file, send_from_directory
 
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from sqlalchemy import desc
+
+from os.path import dirname, join
 from functools import wraps
 from base64 import b64decode
-from os.path import dirname, join
 from time import time
-
-from sqlalchemy import desc
-from sqlalchemy.exc import SQLAlchemyError, IntegrityError
-from datetime import date, datetime, timedelta
-from xml.sax.saxutils import escape
-from flask_cors import CORS
+from os import urandom
+from re import findall
 
 from indoktrinator.site.util import *
-
-import flask
-import os
-import re
 
 
 __all__ = ['make_site']
@@ -37,8 +33,8 @@ def make_site(db, manager, access_model, debug=False, auth=False, cors=False):
     Create the WSGI site object using Flask.
     """
 
-    app = flask.Flask('.'.join(__name__.split('.')[:-1]))
-    app.secret_key = os.urandom(16)
+    app = Flask('.'.join(__name__.split('.')[:-1]))
+    app.secret_key = urandom(16)
     app.debug = debug
 
     if cors:
@@ -48,20 +44,20 @@ def make_site(db, manager, access_model, debug=False, auth=False, cors=False):
     store = manager.store
 
     def has_privilege(privilege):
-        roles = flask.request.headers.get('X-Roles', '')
+        roles = request.headers.get('X-Roles', '')
 
         if not roles or '(null)' == roles:
             roles = ['impotent']
         else:
-            roles = re.findall(r'\w+', roles)
+            roles = findall(r'\w+', roles)
 
         return access_model.have_privilege(privilege, roles)
 
     def pass_user_info(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
-            uid = flask.request.headers.get('X-User-Id', '0')
-            username = flask.request.headers.get('X-Full-Name', 'Someone')
+            uid = request.headers.get('X-User-Id', '0')
+            username = request.headers.get('X-Full-Name', 'Someone')
 
             kwargs.update({
                 'uid': int(uid),
@@ -85,12 +81,12 @@ def make_site(db, manager, access_model, debug=False, auth=False, cors=False):
 
     @app.errorhandler(Forbidden.code)
     def unauthorized(e):
-        return flask.render_template('forbidden.html')
+        return render_template('forbidden.html')
 
     @app.errorhandler(SQLAlchemyError)
     def handle_sqlalchemy_error(error):
         log.msg('SQLAlchemyError: {}'.format(error))
-        response = flask.jsonify({
+        response = jsonify({
             'message': str(error.orig),
         })
         response.status_code = 500
@@ -100,12 +96,12 @@ def make_site(db, manager, access_model, debug=False, auth=False, cors=False):
     @authorized_only('user')
     def index():
         nonlocal has_privilege
-        return flask.render_template('index.html', **locals())
+        return render_template('index.html', **locals())
 
     @app.route('/api/device/', methods=['GET', 'POST'])
     @authorized_only('user')
     def device_handler(**kwargs):
-        if 'GET' == flask.request.method:
+        if 'GET' == request.method:
             devices = []
 
             query = store.device.query() \
@@ -115,149 +111,149 @@ def make_site(db, manager, access_model, debug=False, auth=False, cors=False):
             for device in query:
                 status = manager.devices.get(device['id'], {})
                 devices.append(dict(device, **{
-                    'photo': flask.url_for('device_photo', id=device['id']),
+                    'photo': url_for('device_photo', id=device['id']),
                     'online': status.get('last_seen', 0) > time() - 300,
                     'power': status.get('power', False),
                 }))
 
-            return flask.jsonify(result=devices)
+            return jsonify(result=devices)
 
-        if 'POST' == flask.request.method:
-            device = flask.request.get_json(force=True)
-            return flask.jsonify(manager.device.insert(device))
+        if 'POST' == request.method:
+            device = request.get_json(force=True)
+            return jsonify(manager.device.insert(device))
 
     @app.route('/api/device/<id>', methods=['GET', 'DELETE', 'PATCH'])
     @authorized_only('user')
     def device_item_handler(id, **kwargs):
-        if 'GET' == flask.request.method:
+        if 'GET' == request.method:
             result = manager.device.get_item(id)
-            result['photo'] = flask.url_for('device_photo', id=result['id'])
-            return flask.jsonify(result)
+            result['photo'] = url_for('device_photo', id=result['id'])
+            return jsonify(result)
 
-        if 'DELETE' == flask.request.method:
-            return flask.jsonify(manager.device.delete(id))
+        if 'DELETE' == request.method:
+            return jsonify(manager.device.delete(id))
 
-        if 'PATCH' == flask.request.method:
-            device = flask.request.get_json(force=True)
+        if 'PATCH' == request.method:
+            device = request.get_json(force=True)
             device['id'] = id
-            return flask.jsonify(manager.device.update(device))
+            return jsonify(manager.device.update(device))
 
     @app.route('/api/file/', methods=['GET'])
     @authorized_only('user')
     def file_handler(**kwargs):
-        if 'GET' == flask.request.method:
+        if 'GET' == request.method:
             result = manager.file.list()
             for r in result:
-                r['preview'] = flask.url_for('file_preview', uuid=r['uuid'])
+                r['preview'] = url_for('file_preview', uuid=r['uuid'])
 
-            return flask.jsonify(result=result)
+            return jsonify(result=result)
 
     @app.route('/api/file/<uuid>', methods=['GET'])
     @authorized_only('user')
     def file_item_handler(uuid, **kwargs):
-        if 'GET' == flask.request.method:
+        if 'GET' == request.method:
             r = manager.file.get_item(uuid)
-            r['preview'] = flask.url_for('file_preview', uuid=r['uuid'])
-            return flask.jsonify(r)
+            r['preview'] = url_for('file_preview', uuid=r['uuid'])
+            return jsonify(r)
 
     @app.route('/api/event/', methods=['GET', 'POST'])
     @authorized_only('user')
     def event_handler(**kwargs):
-        if 'GET' == flask.request.method:
-            return flask.jsonify(result=manager.event.list())
+        if 'GET' == request.method:
+            return jsonify(result=manager.event.list())
 
-        if 'POST' == flask.request.method:
+        if 'POST' == request.method:
             try:
-                return flask.jsonify(manager.event.insert(
-                    flask.request.get_json(force=True)
+                return jsonify(manager.event.insert(
+                    request.get_json(force=True)
                 ))
             except IntegrityError as e:
                 # FIXME: Use some generic DB error handling infrastructure.
                 #        Plus this is definitely not a 500 error.
-                response = flask.jsonify({'message': 'Invalid intersection'})
+                response = jsonify({'message': 'Invalid intersection'})
                 response.status_code = 500
                 return response
 
     @app.route('/api/event/<uuid>', methods=['GET', 'DELETE', 'PATCH'])
     @authorized_only('user')
     def event_item_handler(uuid, **kwargs):
-        if 'GET' == flask.request.method:
-            return flask.jsonify(manager.event.get_item(uuid))
+        if 'GET' == request.method:
+            return jsonify(manager.event.get_item(uuid))
 
-        if 'DELETE' == flask.request.method:
-            return flask.jsonify(manager.event.delete(uuid))
+        if 'DELETE' == request.method:
+            return jsonify(manager.event.delete(uuid))
 
-        if 'PATCH' == flask.request.method:
+        if 'PATCH' == request.method:
             try:
-                event = flask.request.get_json(force=True)
+                event = request.get_json(force=True)
                 event['uuid'] = uuid
-                return flask.jsonify(manager.event.update(event))
+                return jsonify(manager.event.update(event))
             except IntegrityError as e:
-                response = flask.jsonify({'message': 'Invalid intersection'})
+                response = jsonify({'message': 'Invalid intersection'})
                 response.status_code = 500
                 return response
 
     @app.route('/api/item/', methods=['GET', 'POST'])
     @authorized_only('user')
     def item_handler(**kwargs):
-        if 'GET' == flask.request.method:
+        if 'GET' == request.method:
             result = manager.item.list()
             for r in result:
-                preview = flask.url_for('file_preview', uuid=r['_file']['id'])
+                preview = url_for('file_preview', uuid=r['_file']['id'])
                 r['_file']['preview'] = preview
 
-            return flask.jsonify(result=result)
+            return jsonify(result=result)
 
-        if 'POST' == flask.request.method:
-            data = flask.request.get_json(force=True)
+        if 'POST' == request.method:
+            data = request.get_json(force=True)
             item = manager.item.insert(data)
-            return flask.jsonify(item)
+            return jsonify(item)
 
     @app.route('/api/item/<uuid>', methods=['GET', 'DELETE', 'PATCH'])
     @authorized_only('user')
     def item_item_handler(uuid, **kwargs):
-        if 'GET' == flask.request.method:
+        if 'GET' == request.method:
             result = manager.item.get_item(uuid)
-            preview = flask.url_for('file_preview', uuid=result['_file']['id'])
+            preview = url_for('file_preview', uuid=result['_file']['id'])
             result['_file']['preview'] = preview
 
-            return flask.jsonify(result)
+            return jsonify(result)
 
-        if 'DELETE' == flask.request.method:
-            return flask.jsonify(manager.item.delete(uuid))
+        if 'DELETE' == request.method:
+            return jsonify(manager.item.delete(uuid))
 
-        if 'PATCH' == flask.request.method:
-            item = flask.request.get_json(force=True)
+        if 'PATCH' == request.method:
+            item = request.get_json(force=True)
             item['uuid'] = uuid
-            return flask.jsonify(manager.item.update(item))
+            return jsonify(manager.item.update(item))
 
     @app.route('/api/playlist/', methods=['GET', 'POST'])
     @authorized_only('user')
     def playlist_handler(**kwargs):
-        if 'GET' == flask.request.method:
-            return flask.jsonify(result=manager.playlist.list())
+        if 'GET' == request.method:
+            return jsonify(result=manager.playlist.list())
 
-        if 'POST' == flask.request.method:
-            playlist = flask.request.get_json(force=True)
+        if 'POST' == request.method:
+            playlist = request.get_json(force=True)
             playlist['system'] = False
-            return flask.jsonify(manager.playlist.insert(playlist))
+            return jsonify(manager.playlist.insert(playlist))
 
     @app.route('/api/playlist/<uuid>', methods=['GET', 'DELETE', 'PATCH'])
     @authorized_only('user')
     def playlist_item_handler(uuid, **kwargs):
-        if 'GET' == flask.request.method:
+        if 'GET' == request.method:
             result = manager.playlist.get_item(uuid)
             for i in result['items']:
-                preview = flask.url_for('file_preview', uuid=i['file']['uuid'])
+                preview = url_for('file_preview', uuid=i['file']['uuid'])
                 i['file']['preview'] = preview
 
-            return flask.jsonify(result)
+            return jsonify(result)
 
-        if 'DELETE' == flask.request.method:
-            return flask.jsonify(manager.playlist.delete(uuid))
+        if 'DELETE' == request.method:
+            return jsonify(manager.playlist.delete(uuid))
 
-        if 'PATCH' == flask.request.method:
-            playlist = flask.request.get_json(force=True)
+        if 'PATCH' == request.method:
+            playlist = request.get_json(force=True)
             playlist['uuid'] = uuid
 
             tmp = manager.playlist.get_item(uuid)
@@ -269,53 +265,53 @@ def make_site(db, manager, access_model, debug=False, auth=False, cors=False):
             if 'path' in playlist and playlist['path']:
                 manager.item.e().filter_by(playlist=uuid).delete()
 
-            return flask.jsonify(manager.playlist.patch(playlist, uuid))
+            return jsonify(manager.playlist.patch(playlist, uuid))
 
     @app.route('/api/playlist/<uuid>/items', methods=['GET'])
     @authorized_only('user')
     def playlist_item_items_handler(uuid, **kwargs):
-        if 'GET' == flask.request.method:
+        if 'GET' == request.method:
             result = manager.item.list({'playlist': uuid})
             for r in result:
-                p = flask.url_for('file_preview', uuid=r['_file']['uuid'])
+                p = url_for('file_preview', uuid=r['_file']['uuid'])
                 r['_file']['preview'] = p
 
-            return flask.jsonify(result)
+            return jsonify(result)
 
     @app.route('/api/program/', methods=['GET', 'POST'])
     @authorized_only('user')
     def program_handler(**kwargs):
-        if 'GET' == flask.request.method:
-            return flask.jsonify(result=manager.program.list(order_by=['name']))
+        if 'GET' == request.method:
+            return jsonify(result=manager.program.list(order_by=['name']))
 
-        if 'POST' == flask.request.method:
-            return flask.jsonify(manager.program.insert(
-                flask.request.get_json(force=True)
+        if 'POST' == request.method:
+            return jsonify(manager.program.insert(
+                request.get_json(force=True)
             ))
 
     @app.route('/api/program/<uuid>', methods=['GET', 'DELETE', 'PATCH'])
     @authorized_only('user')
     def program_item_handler(uuid, **kwargs):
-        if 'GET' == flask.request.method:
-            return flask.jsonify(manager.program.get_item(uuid))
+        if 'GET' == request.method:
+            return jsonify(manager.program.get_item(uuid))
 
-        if 'DELETE' == flask.request.method:
-            return flask.jsonify(manager.program.delete(uuid))
+        if 'DELETE' == request.method:
+            return jsonify(manager.program.delete(uuid))
 
-        if 'PATCH' == flask.request.method:
-            program = flask.request.get_json(force=True)
+        if 'PATCH' == request.method:
+            program = request.get_json(force=True)
             program['uuid'] = uuid
-            return flask.jsonify(manager.program.update(program))
+            return jsonify(manager.program.update(program))
 
     # Segments
     @app.route('/api/segment/', methods=['GET', 'POST'])
     @authorized_only('user')
     def segment_handler(**kwargs):
-        if 'GET' == flask.request.method:
-            return flask.jsonify(result=manager.segment.list())
+        if 'GET' == request.method:
+            return jsonify(result=manager.segment.list())
 
-        if 'POST' == flask.request.method:
-            segment = flask.request.get_json(force=True)
+        if 'POST' == request.method:
+            segment = request.get_json(force=True)
             segment['day'] %= 7
 
             if segment.get('sidebar') and '://' not in segment['sidebar']:
@@ -325,23 +321,23 @@ def make_site(db, manager, access_model, debug=False, auth=False, cors=False):
                 segment['panel'] = 'http://' + segment['panel']
 
             try:
-                return flask.jsonify(manager.segment.insert(segment))
+                return jsonify(manager.segment.insert(segment))
             except IntegrityError as e:
-                response = flask.jsonify({'message': 'Invalid intersection'})
+                response = jsonify({'message': 'Invalid intersection'})
                 response.status_code = 500
                 return response
 
     @app.route('/api/segment/<uuid>', methods=['GET', 'DELETE', 'PATCH'])
     @authorized_only('user')
     def segment_item_handler(uuid, **kwargs):
-        if 'GET' == flask.request.method:
-            return flask.jsonify(manager.segment.get_item(uuid))
+        if 'GET' == request.method:
+            return jsonify(manager.segment.get_item(uuid))
 
-        if 'DELETE' == flask.request.method:
-            return flask.jsonify(manager.segment.delete(uuid))
+        if 'DELETE' == request.method:
+            return jsonify(manager.segment.delete(uuid))
 
-        if 'PATCH' == flask.request.method:
-            segment = flask.request.get_json(force=True)
+        if 'PATCH' == request.method:
+            segment = request.get_json(force=True)
 
             segment['uuid'] = uuid
             segment['day'] %= 7
@@ -353,33 +349,33 @@ def make_site(db, manager, access_model, debug=False, auth=False, cors=False):
                 segment['panel'] = 'http://' + segment['panel']
 
             try:
-                return flask.jsonify(manager.segment.update(segment))
+                return jsonify(manager.segment.update(segment))
             except IntegrityError as e:
-                response = flask.jsonify({'message': 'Invalid intersection'})
+                response = jsonify({'message': 'Invalid intersection'})
                 response.status_code = 500
                 return response
 
     @app.route('/api/user-info/', methods=['GET'])
     @pass_user_info
     def userinfo_handler(**kwargs):
-        return flask.jsonify(kwargs)
+        return jsonify(kwargs)
 
     @app.route('/media/<path:path>')
     def media(path):
         # NOTE: This should never be used outside development.
         #       There should be an actual web server in front of the
         #       application that serves the /media directory faster.
-        return flask.send_from_directory(manager.media_path, path,
-                                         mimetype='application/octet-stream')
+        return send_from_directory(manager.media_path, path,
+                                   mimetype='application/octet-stream')
 
     @app.route('/api/preview-image/device/<id>')
     def device_photo(id):
         device_photo = db.device_photo.filter_by(id=id).one_or_none()
 
         if device_photo is None:
-            return flask.send_file(DEFAULT_DEVICE_PHOTO)
+            return send_file(DEFAULT_DEVICE_PHOTO)
 
-        resp = flask.Response(device_photo.photo)
+        resp = Response(device_photo.photo)
         resp.headers['Content-Type'] = device_photo.mime
         return resp
 
@@ -388,9 +384,9 @@ def make_site(db, manager, access_model, debug=False, auth=False, cors=False):
         file_preview = db.file_preview.filter_by(uuid=uuid).one_or_none()
 
         if file_preview is None:
-            return flask.send_file(DEFAULT_FILE_PREVIEW)
+            return send_file(DEFAULT_FILE_PREVIEW)
 
-        resp = flask.Response(file_preview.preview)
+        resp = Response(file_preview.preview)
         resp.headers['Content-Type'] = file_preview.mime
         return resp
 
