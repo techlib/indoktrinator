71da32599f3561f54d7df328ddcbb4f589dda26c
Jakub Koudelka <koudelka.j@gmail.com>
2017-01-25 15:59:35 +0100

    ui: unified new playlist dialog with new program
    Signed-off-by: Jakub Koudelka <koudelka.j@gmail.com>

--- indoktrinator/static/js/src/components/PlaylistNew.jsx
+++ indoktrinator/static/js/src/components/PlaylistNew.jsx
@@ -3,21 +3,15 @@ import * as Reflux from 'reflux'
 import {PlaylistActions, FeedbackActions} from '../actions'
 import {PlaylistStore} from '../stores/Playlist'
 import {hashHistory as BrowserHistory} from 'react-router'
-import {guid} from '../util/database'
 import {Feedback} from './Feedback'
 import {Input} from 'react-bootstrap'
 import {translate} from 'react-i18next'
 
 export var PlaylistNew = translate(['playlist', 'common'])(React.createClass({
 
-  commonProps: {
-    labelClassName: 'col-xs-2',
-    wrapperClassName: 'col-xs-10',
+  getInitialState() {
+    return {name: ''}
   },
-
-  mixins: [
-    Reflux.connect(PlaylistStore, 'playlist')
-  ],
 
   handleChange(evt) {
     this.setState({[evt.target.name]: evt.target.value})
@@ -48,21 +42,24 @@ export var PlaylistNew = translate(['playlist', 'common'])(React.createClass({
     }
   },
 
+  cancel() {
+    BrowserHistory.push('/playlist/')
+  },
+
   render() {
-    const {t} = this.props
+		const {t} = this.props
 
     return (
-      <div className='col-xs-12 container-fluid'>
-        <h1>
-          {t('playlist:titlenew')}
-        </h1>
+      <div className='col-xs-12 col-sm-6 col-sm-push-3 container-fluid'>
+        <h1></h1>
         <div className='row'>
-          <div className='col-xs-12 col-md-6'>
-            <Feedback />
+          <div className='col-xs-12'>
             <div className='panel panel-default'>
+
               <div className='panel-heading'>
-                {t('playlist:title')}
+                {t('playlist:new.title')}
               </div>
+
               <div className='panel-body'>
                 <div className="form-horizontal">
                   <Input
@@ -72,19 +69,28 @@ export var PlaylistNew = translate(['playlist', 'common'])(React.createClass({
                     name="name"
                     onChange={this.handleChange}
                     value={this.state.name}
-                    {...this.commonProps} />
+                    labelClassName="col-xs-3"
+                    wrapperClassName="col-xs-9"
+                    autoFocus
+                  />
                 </div>
               </div>
+
               <div className='panel-footer'>
                 <div className="row">
-                  <div className="col-xs-6">
+                  <div className="col-xs-12 text-right">
+                    <button className='btn btn-default'
+                      onClick={this.cancel}>
+                      {t('playlist:buttons.new.cancel')}
+                    </button>
                     <button className='btn btn-primary'
                       onClick={this.save}>
-                      {t('playlist:buttons.create')}
+                      {t('playlist:buttons.new.create')}
                     </button>
                   </div>
                 </div>
               </div>
+
             </div>
           </div>
         </div>
