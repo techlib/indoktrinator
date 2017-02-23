2963fa0fcb7bc9a65fe9aa07450af68271758436
Jakub Koudelka <koudelka.j@gmail.com>
2017-02-23 20:12:56 +0100

    ui: removed unused code from event store
    Signed-off-by: Jakub Koudelka <koudelka.j@gmail.com>

--- indoktrinator/static/js/src/stores/Event.js
+++ indoktrinator/static/js/src/stores/Event.js
@@ -1,15 +1,13 @@
 'use strict'
 
 import * as Reflux from 'reflux'
-import {EventActions, FeedbackActions} from '../actions'
+import {EventActions} from '../actions'
 import {ErrorMixin, Api} from './Mixins'
 import {API_URL} from './config'
-import * as _ from 'lodash'
 
 export var EventStore = Reflux.createStore({
   mixins: [ErrorMixin, Api],
   listenables: [EventActions],
-  data: {'list': []},
 
   onDelete(id) {
     this.req('DELETE', `${API_URL}/api/event/${id}`,
