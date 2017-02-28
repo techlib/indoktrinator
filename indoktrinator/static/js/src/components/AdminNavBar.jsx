4d7b405ea1d955745c40308817543862be2de081
Jakub Koudelka <koudelka.j@gmail.com>
2017-02-28 20:10:22 +0100

    ui: removed dead files
    Signed-off-by: Jakub Koudelka <koudelka.j@gmail.com>

--- indoktrinator/static/js/src/components/AdminNavBar.jsx
+++ indoktrinator/static/js/src/components/AdminNavBar.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-//import {UserInfoStore} from "../stores/UserInfo"
 import {Nav, Navbar, NavItem} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
 import {LocaleSwitcher} from './LocaleSwitcher'
