f69a31f5719927a1ca70898961271418bd481b9d
Jakub Koudelka <koudelka.j@gmail.com>
2017-02-28 09:32:28 +0100

    ui: updated playlistdetail to use react-bootstrap
    Signed-off-by: Jakub Koudelka <koudelka.j@gmail.com>

--- indoktrinator/static/js/src/components/PlaylistDetail.jsx
+++ indoktrinator/static/js/src/components/PlaylistDetail.jsx
@@ -1,39 +1,48 @@
 import * as React from 'react'
 import moment from 'moment'
+import {Grid, Col, Row, ListGroup, ListGroupItem} from 'react-bootstrap'
+import {Icon} from './Icon'
 
 export var PlaylistDetail = React.createClass({
 
+  getDuration(item) {
+    return moment.duration(item._file.duration, 'seconds').format('mm:ss', {trim: false})
+  },
+
   render() {
     return (
-      <div className='col-xs-12 container-fluid'>
-        <h1>{this.props.name}</h1>
+      <Grid fluid>
+        <Row>
+          <Col xs={12}>
+            <h1>{this.props.name}</h1>
+            <ListGroup className='list-view-pf list-view-pf-view playlist'>
 
-        <div className="list-group list-view-pf list-view-pf-view playlist">
-
-          {this.props.items.map((item) => {
-            return (
-								<div className="list-group-item">
-									<div className="list-view-pf-main-info">
-										<div className="list-view-pf-left">
-											<img src={item._file.preview} width="80" height="45"
-													 alt="placeholder image"/>
-										</div>
-										<div className="list-view-pf-body">
-											<div className="list-view-pf-description">
-												<div className="list-group-item-heading">
-													{item._file.path}
-												</div>
-											</div>
-											<div className="list-view-pf-additional-info">
-													<i className="fa fa-clock-o"> </i> {moment.duration(item._file.duration, 'seconds').format('mm:ss', {trim: false})}
-											</div>
-										</div>
-									</div>
-								</div>
-						)
-          })}
-        </div>
-      </div>
+            {this.props.items.map((item) => {
+              return (
+                <ListGroupItem>
+                  <div className="list-view-pf-main-info">
+                    <div className="list-view-pf-left">
+                      <img src={item._file.preview} width="80" height="45"
+                           alt="placeholder image"/>
+                    </div>
+                    <div className="list-view-pf-body">
+                      <div className="list-view-pf-description">
+                        <div className="list-group-item-heading">
+                          {item._file.path}
+                        </div>
+                      </div>
+                      <div className="list-view-pf-additional-info">
+                        <Icon fa='clock-o' /> {this.getDuration(item)}
+                      </div>
+                    </div>
+                  </div>
+                  </ListGroupItem>
+              )
+            })}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Grid>
     )
   }
 })
