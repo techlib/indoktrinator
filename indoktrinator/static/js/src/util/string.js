eaffa792ca8e58e4461640ede845f5a1ea4697b3
Jakub Koudelka <koudelka.j@gmail.com>
2017-02-28 13:42:11 +0100

    ui: removed dead files
    Signed-off-by: Jakub Koudelka <koudelka.j@gmail.com>

--- indoktrinator/static/js/src/util/string.js
+++
@@ -1,9 +0,0 @@
-export function add3Dots(string, limit) {
-  var dots = '...'
-  if (string.length > limit) {
-    // you can also use substr instead of substring
-    string = string.substring(0, limit) + dots
-  }
-
-  return string
-}
